456e0c0ac11514e034078af74167f60b6304d1dd
Alek Storm <alek.storm@gmail.com>
2013-11-21 13:32:45 -0800

    Fixup grunt-contrib-jasmine handling in v1.2.0rc1 and 1.2.0-rc.2

--- build/v1.2.0-rc.2.sh
+++ build/v1.2.0-rc.2.sh
@@ -1,2 +1,3 @@
+npm uninstall grunt-contrib-jasmine-node --save-dev
 npm install
 grunt bower buildall minall copy:i18n
--- build/v1.2.0rc1.sh
+++ build/v1.2.0rc1.sh
@@ -1,4 +1,3 @@
 npm uninstall grunt-contrib-jasmine-node --save-dev
-npm install grunt-jasmine-node@~0.1.0
 npm install
 grunt bower buildall minall copy:i18n
